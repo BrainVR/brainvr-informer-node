e186b7437afbb69c192d68c268910cf51a52b38d
hejtmy <hejtmy@gmail.com>
2018-03-23 04:26:05 -0700

    Adds player position reception

--- public/javascripts/incomming.js
+++ public/javascripts/incomming.js
@@ -2,5 +2,9 @@ var socket = io();
 
 socket.on('unityEvent', function (data) {
   console.log(data);
+  document.getElementById('event').innerHTML = String(data);
+});
+
+socket.on('unityPlayer', function(data){
   document.getElementById('position').innerHTML = String(data);
 });
