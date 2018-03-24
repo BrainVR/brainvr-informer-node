ee3a8c9fe6a0f83e7509d480dc7f8da008dc222e
hejtmy <hejtmy@gmail.com>
2018-03-23 23:27:35 -0700

    Adds participant template. Hacks way around passing env variables form server to client js

--- public/javascripts/incomming.js
+++ public/javascripts/incomming.js
@@ -1,10 +1,6 @@
 var socket = io();
-
-socket.on('unityEvent', function (data) {
+console.log(participantCode);
+socket.on(participantCode, function (data) {
   console.log(data);
   document.getElementById('event').innerHTML = String(data);
-});
-
-socket.on('unityPlayer', function(data){
-  document.getElementById('position').innerHTML = String(data);
 });
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'BrainVR Informer' });
 });
 
+router.get('/participant/:id', function(req, res, next){
+  res.render('participant', {participantCode: req.params.id});
+});
+
 router.post('/', function(req, res, next){
   console.log(req.body);
   var type = Object.keys(req.body)[0];
+
   if (type == 'event'){
     io.emit('unityEvent', req.body[[type]]);
   }
